165dfeabaf5a94377425180fc05e630b84daf70d
Georgel03 <gstance@ymail.com>
2023-04-10 10:30:11 -0700

    Add categories / Game

--- src/components/common/Select.js
+++ src/components/common/Select.js
@@ -6,26 +6,30 @@ import FormControl from '@mui/material/FormControl';
 import Select from '@mui/material/Select';
 
 export default function BasicSelect() {
-  const [age, setAge] = React.useState('');
+  const [gameType, setGameType] = React.useState('');
 
   const handleChange = (event) => {
-    setAge(event.target.value);
+    setGameType(event.target.value);
   };
 
   return (
-    <Box sx={{ minWidth: 120 }}>
+    <Box sx={{ minWidth: 220 }}>
       <FormControl fullWidth>
-        <InputLabel id="demo-simple-select-label">Age</InputLabel>
+        <InputLabel id="demo-simple-select-label">Game Category</InputLabel>
         <Select
           labelId="demo-simple-select-label"
           id="demo-simple-select"
-          value={age}
-          label="Age"
+          value={gameType}
+          label="Game Category"
           onChange={handleChange}
         >
-          <MenuItem value={10}>Ten</MenuItem>
-          <MenuItem value={20}>Twenty</MenuItem>
-          <MenuItem value={30}>Thirty</MenuItem>
+          <MenuItem value={9}>General culture</MenuItem>
+          <MenuItem value={10}>Entertainment: Books</MenuItem>
+          <MenuItem value={11}>Entertainment: Film</MenuItem>
+          <MenuItem value={12}>Entertainment: Music</MenuItem>
+          <MenuItem value={13}>Entertainment: Opera and Theatres</MenuItem>
+          <MenuItem value={14}>Entertainment: TV</MenuItem>
+          <MenuItem value={15 }>Entertainment: Board games</MenuItem>
         </Select>
       </FormControl>
     </Box>
