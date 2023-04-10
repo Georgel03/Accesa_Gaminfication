40311d5233a56b10da497a012963dd8d229d1b64
Georgel03 <gstance@ymail.com>
2023-04-10 10:02:16 -0700

    Add module / Game

--- src/components/Game.js
+++ src/components/Game.js
@@ -2,8 +2,13 @@ import React from 'react'
 
 export default function Game() {
   return (
-    <div>
+    <div className='app-main'>
         <h1>Gamification</h1>
+
+        <input placeholder='Question number'/>
+        <input placeholder='Category number'/>
+        <input placeholder='Level of dificulty'/>
+        
     </div>
   )
 }
