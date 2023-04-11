aa6ce706453b31f467837c2571656234889485a9
Georgel03 <gstance@ymail.com>
2023-04-11 02:00:08 -0700

    Add Retry button / Result

--- src/components/common/Result.js
+++ src/components/common/Result.js
@@ -1,6 +1,11 @@
-import React, { useState } from 'react'
-import { useLocation } from 'react-router-dom'
+import React from 'react'
+import { useLocation } from 'react-router-dom';
+import Button from '@mui/material/Button';
+import  { useNavigate } from 'react-router-dom'
+
+
 export default function Result() {
+  const navigate = useNavigate();
   const { state } = useLocation();
   const [finalResult, setFinalResult] = React.useState(0)
   React.useEffect(() => {
@@ -8,11 +13,23 @@ export default function Result() {
      setFinalResult(finalResults)
 
   }, [])
+  const retryGame= () => {
+        navigate('/')
+  }
   return (
     <div>
        <h1>Results</h1>
 
        <h2>Final score is : {finalResult} </h2> 
+
+       <Button 
+
+        onClick={retryGame}
+        variant="contained" 
+        style={{marginLeft: 10 }}>
+        Play Again! 
+
+       </Button>
     </div>
   )
 }
